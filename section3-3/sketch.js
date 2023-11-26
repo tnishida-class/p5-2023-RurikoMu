5290772549697b54dad222d7f7bbda82b9ecf9b2
RurikoMu <146911068+RurikoMu@users.noreply.github.com>
2023-11-26 22:19:43 +0900

    final work

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -2,13 +2,14 @@
 let x, y, vx, vy;
 const g = 1; // 重力
 const vyMax = 20;
+let isJumping = false;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height * 3 / 4 - 6;
   vx = 2;
-  vy = 2;
+  vy = 10;
   blocksize = 50
 }
 
@@ -18,7 +19,6 @@ function draw(){
   y += vy; //重力
   x = constrain(x, 0, width,);
   y = constrain(y, 0, height * 3 / 4,);
-  const block1 = { x: 200, y: height * 3 / 4 - 150, w: blocksize * 2, h: blocksize};
   
   noStroke();
   fill(128, 88, 77);
@@ -28,16 +28,10 @@ function draw(){
 
   //雲
   cloud(145, 100, 270, 60);
-  cloud(145 + 120, 125, 100, 40);
+  cloud(500, 350, 270, 60);
+  cloud(900, 250, 270, 60);
+  cloud(1300, 150, 270, 60);
 
-  cloud(600, 350, 270, 60);
-  cloud(600 - 120, 375, 100, 40);
-
-  cloud(1000, 250, 270, 60);
-  cloud(1000 - 120, 225, 100,40);
-
-  cloud(1400, 150, 270, 60);
-  cloud(1400 - 120, 175, 100, 40);
 
   //花
   for(let i =0; i < 10; i++){
@@ -55,31 +49,53 @@ function draw(){
   }
   }
   
-
-  
+  //ブロック
+  const block1 = { x: 200, y: height * 3 / 4 - 150, w: blocksize * 2, h: blocksize};
+  const block2 = { x: 600, y: height * 3 / 4 - 150, w: blocksize * 3, h: blocksize};
+  block(block1.x, block1.y, block1.w, block1.h);
+  block(block2.x, block2.y, block2.w, block2.h);
 
   //まるお
   stroke(0);
   strokeWeight(0.5);
   fill(255);
-  ellipse(x, y - 25, 50) ; //半径ぶんy引く
-  let speed = 5;
-  if(keyIsDown("S".charCodeAt(0))){ speed = 10; }
-  if(keyIsDown(LEFT_ARROW)){ x -= speed; }
-  if(keyIsDown(RIGHT_ARROW)){ x += speed;  }
-  //if(keyIsDown(UP_ARROW)){ y -= 5; }
+  ellipse(x, y - 25, 50) ; //地面にめり込まないよう半径ぶんy引く
+  let vx = 5;
+  if(keyIsDown(SHIFT)){ vx = 10; } //加速
+  if(keyIsDown(LEFT_ARROW)){ x -= vx; }
+  if(keyIsDown(RIGHT_ARROW)){ x += vx;  }
+  //if(keyIsDown(UP_ARROW)){}
   //if(keyIsDown(DOWN_ARROW)){ y += 5; }
 
-block(block1.x, block1.y, block1.w, block1.h);
+console.log(isJumping)
 }
 
+//ジャンプ
+function keyPressed(){
+  if(key == " " || key === 'ArrowUp'){
+    jump();
+    isJumping = true; 
+  }
+}
 
+function jump(){
+  if(isJumping === false){
+  vy= -vy}
+}
+
+function keyReleased(){
+  if (key === " " || key === "ArrowUp"){
+    isJumping = false;
+  }
+}
+
+//ブロック関数
 function block(bx, by, bw, bh){
   stroke(0);
   strokeWeight(1);
   fill(128, 88, 77);
   rect(bx, by, bw, bh);
-  
+
   //ブロックの下でジャンプ中
   if(bx - 25 < x &&  x < bx +  bw + 25 &&  by + bh < y  && y < by + bh + 50 && vy < 0){
     y = by + bh + 50;
@@ -87,14 +103,14 @@ function block(bx, by, bw, bh){
   }
   //ブロックの上
   else if(x > bx - 25 && x < bx + bw + 25 && by < y && y < by + bh){
-    y = by;
-    vy = 2;
+    y = constrain(y, height / 3, by);
+    vy = 10;
   //ブロック上でジャンプ
-    if(keyIsDown(" ".charCodeAt(0))){
-    vy = -20; 
+    if(keyIsDown(" ".charCodeAt(0)) || keyIsDown(UP_ARROW) ){
+    jump(); 
     }
     else {
-      y = constrain(y, height * 2 / 4, height * 3 / 4,);
+      y = constrain(y, height * 1 / 4, height * 3 / 4,);
     }
     }
   //地面にいるとき
@@ -103,28 +119,16 @@ function block(bx, by, bw, bh){
   }
 }
 
-
-
-//ジャンプ
-
-function keyPressed(){
-  if(key == " "){
-    jump();
-  }
-}
-
-function jump(){
-  if(y == height * 3 / 4 || y == height * 3 / 4 - 150);
-  vy= -vy
-}
-
-
+//雲関数
 function cloud(x, y, w ,h){
   noStroke();
   fill(255);
   ellipse(x, y, w, h);
+  ellipse(x + 120, y + 25, w - 170, h - 20);
 }
 
+
+//花関数
 function flower(x, y, r){
   stroke(107, 181, 108);
   line(x, y, x, y + 50);
